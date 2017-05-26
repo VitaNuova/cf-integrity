c7a31cbe3ef423342b63047bca182fc3aed54a6a
VitaNuova <vitanuova@gmail.com>
2017-05-26 19:45:22 +0000

    added hash calculation

--- whitelist/WhiteList.cpp
+++ whitelist/WhiteList.cpp
@@ -18,13 +18,23 @@ namespace {
    struct WhiteListPass: public ModulePass {
       static char ID;
       WhiteListPass(): ModulePass(ID) {}
+      
+      unsigned calculate_hash(vector<string> whitelist) {
+         unsigned hash = 0;
+     	 for(string fname: whitelist) {
+	    for(char fchar: fname) {
+	       hash += fchar;
+	    }
+	 }
+	 errs() << "Hash value: " << hash << '\n';
+	 return hash;
+      }
 
       bool runOnModule(Module &M) override {
-	for(string f: Param) {
-		errs() << "Sensitive function: " << f << '\n';	
-	}
- 
- 		//CallGraphWrapperPass* pass = getAnalysisIfAvailable<CallGraphWrapperPass>();
+         for(string f: Param) {
+	    errs() << "Sensitive function: " << f << '\n';	
+	 }
+  		//CallGraphWrapperPass* pass = getAnalysisIfAvailable<CallGraphWrapperPass>();
 		// const CallGraph& graph = (*pass).getCallGraph();
 		// const CallGraph& graph = SCC.getCallGraph();
 		 /*for(const pair<const llvm::Function* const, unique_ptr<llvm::CallGraphNode>>& node: graph) {
@@ -62,10 +72,30 @@ namespace {
 			}
 			
 		}*/
-		 return false;
-	      }
-	   };
-	}
+          vector<vector<string>> whitelists;
+		
+          vector<string> whitelist1;
+          whitelist1.push_back("main");
+     	  whitelist1.push_back("operation2");
+	  whitelist1.push_back("encrypt");
+	  whitelist1.push_back("readKey");
+          whitelists.push_back(whitelist1);
+		
+	  vector<string> whitelist2;
+	  whitelist2.push_back("main");
+	  whitelist2.push_back("operation2");
+	  whitelist2.push_back("decrypt");
+	  whitelist2.push_back("readKey");
+	  whitelists.push_back(whitelist2);
+
+	  for(vector<string> whitelist: whitelists) {
+	     unsigned hash = calculate_hash(whitelist);
+	  }
+      return false;
+      }
+   };
+}
+
 
 	char WhiteListPass::ID = 0;
 
